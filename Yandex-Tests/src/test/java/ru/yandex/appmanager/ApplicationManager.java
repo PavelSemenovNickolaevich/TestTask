0a6c7bdc1c4cf64efbafd4eae8276ec992f43c76
test <natitshow@gmail.com>
2020-04-18 23:45:04 +0300

    refactor

--- Yandex-Tests/src/test/java/ru/yandex/appmanager/ApplicationManager.java
+++ Yandex-Tests/src/test/java/ru/yandex/appmanager/ApplicationManager.java
@@ -56,4 +56,40 @@ public class ApplicationManager {
             acceptNextAlert = true;
         }
     }
+
+    public void findProduct () {
+        wd.findElement(By.xpath("//button[@type='submit']")).click();
+    }
+
+    public void selectOption () {
+        wd.findElement(By.xpath("//div[@id='search-prepack']/div/div/div[3]/div/div/div[2]/div[2]/div/div/fieldset/ul/li[6]/div/a/label/div")).click();
+        wd.findElement(By.xpath("//div[@id='search-prepack']/div/div/div[3]/div/div/div[2]/div[2]/div/div/fieldset/ul/li[8]/div/a/label/div")).click();
+        wd.findElement(By.xpath("(//button[@type='button'])[2]")).click();
+        wd.findElement(By.xpath("//div[@id='uniq15872317257651']/span")).click();
+    }
+
+    public void changeWindow () {
+        wd.switchTo().window("https://market.yandex.ru/");
+    }
+
+    public void selectProduct () {
+        wd.findElement(By.cssSelector("div._1vnugfYUli")).click();
+        wd.findElement(By.linkText("\"Ноутбуки\"")).click();
+    }
+
+    public void setProduct () {
+        wd.findElement(By.id("header-search")).click();
+        wd.findElement(By.id("header-search")).clear();
+        wd.findElement(By.id("header-search")).sendKeys("Ноутбук HP 15-db0");
+    }
+
+    public void setHighPrice () {
+        wd.findElement(By.id("glpriceto")).click();
+        wd.findElement(By.id("glpriceto")).clear();
+        wd.findElement(By.id("glpriceto")).sendKeys("30000");
+    }
+
+    public void findYandexMarket () {
+        wd.findElements(By.cssSelector("div.organic__url-text")).get(0).click();
+    }
 }
--- Yandex-Tests/src/test/java/ru/yandex/tests/YandexScriptsTestOne.java
+++ Yandex-Tests/src/test/java/ru/yandex/tests/YandexScriptsTestOne.java
@@ -7,51 +7,14 @@ import org.openqa.selenium.*;
 public class YandexScriptsTestOne extends TestBase {
 
     @Test
-    public void testYandex() throws Exception {
-        findYandexMarket();
-        changeWindow();
-        selectProduct(By.cssSelector("div._1vnugfYUli"), By.linkText("Ноутбуки"));
-        setHighPrice("glpriceto", "30000");
-        selectOption();
-        setProduct("header-search", "Ноутбук HP 15-db0");
-        findProduct("//button[@type='submit']");
+    public void testYandex () throws Exception {
+        applicationManager.findYandexMarket();
+        applicationManager.changeWindow();
+        applicationManager.selectProduct();
+        applicationManager.setHighPrice();
+        applicationManager.selectOption();
+        applicationManager.setProduct();
+        applicationManager.findProduct();
     }
-
-    private void findProduct (String s) {
-        applicationManager.wd.findElement(By.xpath(s)).click();
-    }
-
-    private void selectOption () {
-        applicationManager.wd.findElement(By.xpath("//div[@id='search-prepack']/div/div/div[3]/div/div/div[2]/div[2]/div/div/fieldset/ul/li[6]/div/a/label/div")).click();
-        applicationManager.wd.findElement(By.xpath("//div[@id='search-prepack']/div/div/div[3]/div/div/div[2]/div[2]/div/div/fieldset/ul/li[8]/div/a/label/div")).click();
-        applicationManager.wd.findElement(By.xpath("(//button[@type='button'])[2]")).click();
-        applicationManager.wd.findElement(By.xpath("//div[@id='uniq15872317257651']/span")).click();
-    }
-
-    private void changeWindow () {
-        applicationManager.wd.switchTo().window("https://market.yandex.ru/");
-    }
-
-    private void selectProduct (By by, By ноутбуки) {
-        applicationManager.wd.findElement(by).click();
-        applicationManager.wd.findElement(ноутбуки).click();
-    }
-
-    private void setProduct (String s, String s2) {
-        applicationManager.wd.findElement(By.id(s)).click();
-        applicationManager.wd.findElement(By.id(s)).clear();
-        applicationManager.wd.findElement(By.id(s)).sendKeys(s2);
-    }
-
-    private void setHighPrice (String glpriceto, String s) {
-        applicationManager.wd.findElement(By.id(glpriceto)).click();
-        applicationManager.wd.findElement(By.id(glpriceto)).clear();
-        applicationManager.wd.findElement(By.id(glpriceto)).sendKeys(s);
-    }
-
-    private void findYandexMarket () {
-        applicationManager.wd.findElements(By.cssSelector("div.organic__url-text")).get(0).click();
-    }
-
 
 }
